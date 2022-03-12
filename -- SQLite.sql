27acf1f3a2fcb2b078e0b9c22e39288409263b0e
Rin-1108 <rin.codegym@gmail.com>
2022-03-13 01:55:07 +0900

    テーブルを作成

--- -- SQLite.sql
+++ -- SQLite.sql
@@ -1,8 +1,62 @@
 -- SQLite
-CREATE TABLE users(
- id INTEGER PRIMARY KEY AUTOINCREMENT,
- name TEXT,
- password INTEGER,
- is_customer BOOLEAN,
- created_date TIMESTAMP DEFAULT (datetime('now','localtime'))
+-- CREATE TABLE users(
+--  id INTEGER PRIMARY KEY AUTOINCREMENT,
+--  name TEXT,
+--  password INTEGER,
+--  is_customer BOOLEAN,
+--  created_date TIMESTAMP DEFAULT (datetime('now','localtime'))
+-- );
+
+-- DROP TABLE users;
+
+-- CREATE TABLE users(
+--     id INTEGER PRIMARY KEY AUTOINCREMENT,
+--     username TEXT NOT NULL,
+--     hash TEXT NOT NULL,
+--     is_business BOOLEAN NOT NULL,
+--     created_date TIMESTAMP DEFAULT (datetime('now','localtime'))
+-- );
+
+-- CREATE TABLE secrets(
+--     id INTEGER PRIMARY KEY AUTOINCREMENT,
+--     user_id INTEGER NOT NULL,
+--     title TEXT NOT NULL,
+--     content TEXT NOT NULL,
+--     image TEXT NOT NULL,
+--     is_deleted BOOLEAN NOT NULL,
+--     created_date TIMESTAMP DEFAULT (datetime('now','localtime')),
+--     FOREIGN KEY(user_id) REFERENCES users(id)
+-- );
+
+-- CREATE TABLE dreams(
+--     id INTEGER PRIMARY KEY AUTOINCREMENT,
+--     user_id INTEGER NOT NULL,
+--     content TEXT NOT NULL,
+--     is_solved BOOLEAN NOT NULL,
+--     is_deleted BOOLEAN NOT NULL,
+--     created_date TIMESTAMP DEFAULT (datetime('now','localtime')),
+--     FOREIGN KEY(user_id) REFERENCES users(id)
+-- );
+
+-- CREATE TABLE coments(
+--     id INTEGER PRIMARY KEY AUTOINCREMENT,
+--     user_id INTEGER NOT NULL,
+--     dreams_id INTEGER NOT NULL,
+--     content TEXT NOT NULL,
+--     is_best BOOLEAN NOT NULL,
+--     is_deleted BOOLEAN NOT NULL,
+--     created_date TIMESTAMP DEFAULT (datetime('now','localtime')),
+--     FOREIGN KEY(user_id) REFERENCES users(id),
+--     FOREIGN KEY(dreams_id) REFERENCES dreams(id)
+-- );
+
+CREATE TABLE replies(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    coments_id INTEGER NOT NULL,
+    content TEXT NOT NULL,
+    is_deleted BOOLEAN NOT NULL,
+    created_date TIMESTAMP DEFAULT (datetime('now','localtime')),
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(coments_id) REFERENCES coments(id)
 );
