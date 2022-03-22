fcc92755dd481bf4d055078330dbee5d8c0e62b9
Rin-1108 <rin.codegym@gmail.com>
2022-03-22 21:19:50 +0900

    編集機能の仮実装

--- my_final_project/-- SQLite.sql
+++ my_final_project/-- SQLite.sql
@@ -1,2 +1,6 @@
 -- SQLite
-DELETE FROM secrets WHERE id = 1 OR id = 2;
+-- DELETE FROM secrets WHERE id = 1 OR id = 2;
+
+-- INSERT INTO comments (user_id, dreams_id, content, is_best, is_deleted) VALUES ("2", "3", "こんなのありますよー", "1", "0")
+
+DELETE FROM comments WHERE id = 7;
